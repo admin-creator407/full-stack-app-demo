1f999c4d9f3f1473d369c98d8eb225b83bcf27c2
NT <admakeit21@gmail.com>
2026-06-12 23:00:42 +0530

    Frist step to deploy

---
+++ backend/index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5174",
+      "http://localhost:5173",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST"],
+  }),
+);
+
+app.get("/users", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users",
+    );
+
+    return res.json({
+      users: response.data,
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      message: "Something went wrong",
+      error: error.message,
+    });
+  }
+});
+
+app.listen(4000, "0.0.0.0", () =>
+  console.log("The server is running at http://localhost:4000/users"),
+);
---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const data = await axios.get("http://localhost:4000/users");
+
+      setUsers(data.data.users);
+    }
+
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <h1>User Data</h1>
+
+      {users.map((user) => (
+        <h2 key={user.id}>{user.name}</h2>
+      ))}
+    </div>
+  );
+};
+
+export default App;
